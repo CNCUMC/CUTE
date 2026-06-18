ca82ffd1ef59a07b87f4a7b58b44451cb76b9f88
Black_Moss <kytheminecraft@outlook.com>
2026-06-18 20:53:40 +0800

    Initial commit: CUTE mod

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace CUTE;
+
+[BepInPlugin(Guid, Name, Version)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string Guid = "org.cncumc.cute";
+    public const string Name = "CUTE";
+    public const string Version = "1.0.0";
+    internal new static ManualLogSource Logger;
+    private readonly Harmony _harmony = new(Guid);
+
+    public void Awake()
+    {
+        Logger = base.Logger;
+        _harmony.PatchAll();
+
+        Logger.LogInfo("CUTE loaded!");
+    }
+}
