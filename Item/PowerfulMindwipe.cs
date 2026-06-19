c526d036e4c48c54a8cf5d457fd7611644d1a93d
Black_Moss <kytheminecraft@outlook.com>
2026-06-19 09:42:57 +0800

    w

---
+++ Item/PowerfulMindwipe.cs
@@ -0,0 +1,31 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace CUTE.Item;
+
+public static class PowerfulMindwipe
+{
+    private static PlayerCamera _playerCamera;
+    
+    [HarmonyPatch(typeof(MindwipeScript), "Update")]
+    [HarmonyPostfix]
+    private static void UpdatePostfix(MindwipeScript __instance)
+    {
+        if (__instance.active && Plugin.PowerfulMindwipe.Value)
+        {
+            // 持续压制智力为 0
+            _playerCamera.body.skills.INT = (int)Mathf.MoveTowards(
+                _playerCamera.body.skills.INT,
+                0f,
+                Time.deltaTime * 5f
+            );
+        }
+    }
+    
+    [HarmonyPatch(typeof(PlayerCamera), "Start")]
+    [HarmonyPostfix]
+    private static void GetPlayerCamera(PlayerCamera __instance)
+    {
+       _playerCamera = __instance;
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,6 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using CUTE.Lang;
 using HarmonyLib;
@@ -14,6 +16,10 @@ public class Plugin : BaseUnityPlugin
     public const string Version = "1.0.0";
     internal new static ManualLogSource Logger;
     private readonly Harmony _harmony = new(Guid);
+    private static readonly Dictionary<string, ConfigEntryBase> ConfigRegistry = new();
+
+    // Item
+    public static ConfigEntry<bool> PowerfulMindwipe;
 
     public void Awake()
     {
@@ -26,5 +32,22 @@ public class Plugin : BaseUnityPlugin
         
         ModLocale.Initialize(Logger);
         _harmony.PatchAll();
+
+        PowerfulMindwipe = RegisterConfigItem(Config, "powerful_mindwipe", true);
+    }
+    
+    private static ConfigEntry<T> RegisterConfigItem<T>(ConfigFile configFile, string key, T defaultValue)
+    {
+        return RegisterConfig(configFile, "Item", key, defaultValue);
+    }    
+    private static ConfigEntry<T> RegisterConfig<T>(ConfigFile configFile, string section, string key, T defaultValue)
+    {
+        return MossLib.Tool.Config.Register(configFile, section, key, defaultValue,
+            _ => Locale($"config.{section}.{key}.description"), ConfigRegistry);
+    }
+
+    private static string Locale(string key)
+    {
+        return ModLocale.GetFormat(key);
     }
 }
